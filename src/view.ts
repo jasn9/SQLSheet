4babbb4a96c262e1d1c737cd1a2f1b4f6d7581de
Jatin Gupta <jatin9gupta@gmail.com>
2020-12-31 17:55:24 +0530

    refractor

--- src/view.ts
+++ src/view.ts
@@ -4,9 +4,9 @@ import { Constants } from './constants'
 import { GetDatabases, GetTablesRequest } from './databaseConnector'
 
 const databases_link = document.querySelector('#Databases') as HTMLLinkElement
-const databases = document.querySelector('#Databases-div') as HTMLDivElement
+const databasesDiv = document.querySelector('#Databases-div') as HTMLDivElement
 const tables_link = document.querySelector('#Tables') as HTMLLinkElement
-const tables = document.querySelector('#Tables-div') as HTMLDivElement
+const tablesDiv = document.querySelector('#Tables-div') as HTMLDivElement
 
 const add_to_list_tag = (name: string, class_name: string,  src: HTMLElement)=>{
     src.innerHTML += `<a class="dropdown-item ${class_name}" href="#"> ${name} </a>`
@@ -16,19 +16,30 @@ const get_table_name_from_response = (database_name: string|undefined): string =
     return 'Tables_in_'+database_name
 }
 
-const get_database_request = (element: HTMLLinkElement):GetTablesRequest=>{
+const get_database_request = (table: HTMLLinkElement):GetTablesRequest=>{
     const getDatabaseRequest: GetTablesRequest = {
-        Database: element.textContent?.trim()
+        Database: table.textContent?.trim()
     }
     return getDatabaseRequest
 }
 
+const update_tables_tag = (tables: NodeListOf<HTMLElement>)=>{
+    tables.forEach((table: HTMLElement)=>{
+        table.addEventListener('click', (e: Event)=>{
+          const table_element = e.currentTarget as HTMLElement
+          console.log(table_element)   
+        })
+    })
+}
+
 const get_tables_response = (getDatabaseRequest: GetTablesRequest)=>{
-    tables.innerHTML = ""
+    tablesDiv.innerHTML = ""
     ipcRenderer.invoke(Constants.GET_TABLES, getDatabaseRequest).then(
-        (value: any)=>{
-            value.forEach((element:any) => {
-            add_to_list_tag(element[get_table_name_from_response(getDatabaseRequest.Database)], 'Table', tables)
+        (tables: any)=>{
+            tables.forEach((element:any) => {
+            add_to_list_tag(element[get_table_name_from_response(getDatabaseRequest.Database)], 'Table', tablesDiv)
+            const tablesList = document.querySelectorAll('.Table') as NodeListOf<HTMLLinkElement>
+            update_tables_tag(tablesList)
         });
         },
         (error: string)=>{
@@ -37,9 +48,9 @@ const get_tables_response = (getDatabaseRequest: GetTablesRequest)=>{
     )
 }
 
-const update_tables_list = (database: NodeListOf<HTMLElement>)=>{
-    database.forEach((value: HTMLElement, key: number, parent: NodeListOf<HTMLElement>)=>{
-        value.addEventListener('click', (e: Event)=>{
+const update_tables_list = (databases: NodeListOf<HTMLElement>)=>{
+    databases.forEach((database: HTMLElement, key: number, parent: NodeListOf<HTMLElement>)=>{
+        database.addEventListener('click', (e: Event)=>{
             const getDatabaseRequest = get_database_request(e.currentTarget as HTMLLinkElement)
             get_tables_response(getDatabaseRequest)
         })
@@ -47,10 +58,10 @@ const update_tables_list = (database: NodeListOf<HTMLElement>)=>{
 }
 
 ipcRenderer.invoke(Constants.GET_DATABASES).then(
-    (value: GetDatabases[])=>{
-        databases.innerHTML = ""
-        value.forEach((element: GetDatabases) => {
-            add_to_list_tag(element.Database, 'Database', databases)
+    (getDatabases: GetDatabases[])=>{
+        databasesDiv.innerHTML = ""
+        getDatabases.forEach((getDatabase: GetDatabases) => {
+            add_to_list_tag(getDatabase.Database, 'Database', databasesDiv)
         })
         const database = document.querySelectorAll('.Database') as NodeListOf<HTMLElement>
         update_tables_list(database)
